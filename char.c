ef54cdc07fb105a09cc5fcc14b3ca60e5c24c8a2
prAyAgkhArel <prayagkharel006@gmail.com>
2023-10-12 22:09:08 +0545

    first commit

---
+++ char.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+   char a='5';
+   printf("%c\n",a);
+  
+  
+   char b=16; //Now 16 is treated as ascii value and character in that value is printed.
+   printf("%c",b);
+
+
+
+    return 0;
+}
